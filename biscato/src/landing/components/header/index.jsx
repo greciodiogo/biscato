d59734072f87be4319001bf756fecabbf7cf571d
greciodiogo <fonebahia8@gmail.com>
2022-05-19 20:18:39 +0100

    The first container on landing

---
+++ biscato/src/landing/components/header/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-top">
+        <a href="#logo" className="logo">
+          biscato
+        </a>
+        <div className="buttons">
+          <button className="btn outlined">continuar como Convidado</button>
+          <button className="btn contained">Entrar</button>
+        </div>
+      </div>
+      <div className="header-bottom">
+        <div className="header-content">
+          <h2>Venha desenvolver o seu potencial</h2>
+          <h3>A riqueza de um homem é produto da sua capacidade de pensar</h3>
+        </div>
+        <div className="buttons">
+          <button className="btn-large contained">continuar</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
