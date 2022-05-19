c6cf39faef9294753ecbed66c570af8a0e2eff74
greciodiogo <fonebahia8@gmail.com>
2022-05-19 20:16:22 +0100

    This home is the landing page

---
+++ biscato/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer } from '../components'
+import { Header } from '../landing/components'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Homepage
